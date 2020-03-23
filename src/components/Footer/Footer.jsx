ac83457f1ef5780f7b8688e8754a434a3251bcb6
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-03-24 01:28:25 +0200

    mod

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,43 +9,45 @@ import { ReactComponent as Instagram } from "../../assets/svg/instagram.svg";
 const Footer = () => (
   <div className={footerWrapper}>
     <h3>Contacts</h3>
-    <ul>
-      <li>
-        <p>Phone number</p>
-        <a href="tel:+380996267468">(099)626-74-68</a>
-      </li>
-      <li>
-        <p>E-mail</p>
-        <p>valyslav.chychykalo@gmail.com</p>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <a href="https://t.me/Vladyslav_Chychykalo">
-          <Telegram className={socialLink} />
-        </a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/vladyslav-chychykalo-b045a3166/">
-          <LinkedIn className={socialLink} />
-        </a>
-      </li>
-      <li>
-        <a href="https://www.facebook.com/profile.php?id=100010711830394">
-          <Facebook className={socialLink} />
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/VladyslavChychykalo">
-          <GitHub className={socialLink} />
-        </a>
-      </li>
-      <li>
-        <a href="https://www.instagram.com/vlados4agd/?hl=ru">
-          <Instagram className={socialLink} />
-        </a>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <p>Phone number</p>
+          <a href="tel:+380996267468">(099)626-74-68</a>
+        </li>
+        <li>
+          <p>E-mail</p>
+          <p>valyslav.chychykalo@gmail.com</p>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="https://t.me/Vladyslav_Chychykalo">
+            <Telegram className={socialLink} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/vladyslav-chychykalo-b045a3166/">
+            <LinkedIn className={socialLink} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/profile.php?id=100010711830394">
+            <Facebook className={socialLink} />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/VladyslavChychykalo">
+            <GitHub className={socialLink} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/vlados4agd/?hl=ru">
+            <Instagram className={socialLink} />
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
