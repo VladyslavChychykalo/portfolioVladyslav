386f4429b5fe5e1f2a1ad431f30250a1c964c189
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-04-03 12:54:27 +0300

    Updates

---
+++ precache-manifest.12ba37081add99fcdc2a108a6383c4e2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7f36ed354c69d21373574115de873f",
+    "url": "/portfolioVladyslav/index.html"
+  },
+  {
+    "revision": "fab42e2adf31fadfd7ad",
+    "url": "/portfolioVladyslav/static/css/main.ad278fe8.chunk.css"
+  },
+  {
+    "revision": "e4c45d06cf2c88716bc7",
+    "url": "/portfolioVladyslav/static/js/2.e45cb3fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolioVladyslav/static/js/2.e45cb3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab42e2adf31fadfd7ad",
+    "url": "/portfolioVladyslav/static/js/main.55391543.chunk.js"
+  },
+  {
+    "revision": "a0ed62fd8f3909de5b40",
+    "url": "/portfolioVladyslav/static/js/runtime-main.858c8c6c.js"
+  },
+  {
+    "revision": "e93dc08e7ffd91b28f7c0a92f94e4448",
+    "url": "/portfolioVladyslav/static/media/ev-hub.e93dc08e.PNG"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/portfolioVladyslav/static/media/facebook.422e7665.svg"
+  },
+  {
+    "revision": "c40382edb6ecf6348a5a092ebd945202",
+    "url": "/portfolioVladyslav/static/media/filmoteka.c40382ed.PNG"
+  },
+  {
+    "revision": "a112a001d5170d13d5f46d9387e526a4",
+    "url": "/portfolioVladyslav/static/media/github.a112a001.svg"
+  },
+  {
+    "revision": "fa1d9b71edc9ff4f85fa0d5683869148",
+    "url": "/portfolioVladyslav/static/media/headerWrapper.fa1d9b71.jpg"
+  },
+  {
+    "revision": "661c381ab4ee6d47b20355b0a07ef652",
+    "url": "/portfolioVladyslav/static/media/imageFinder.661c381a.PNG"
+  },
+  {
+    "revision": "91a8e68dd3c119bb22f52aaff450c3b9",
+    "url": "/portfolioVladyslav/static/media/instagram.91a8e68d.svg"
+  },
+  {
+    "revision": "89dcd6955d20dc3183df9d8ba408312c",
+    "url": "/portfolioVladyslav/static/media/linkedin.89dcd695.svg"
+  },
+  {
+    "revision": "fcc97e40150b10386c9db9ae3d7e2b91",
+    "url": "/portfolioVladyslav/static/media/movieFinder.fcc97e40.PNG"
+  },
+  {
+    "revision": "a6f053d45f845ec4e5e2b4cfd3daa291",
+    "url": "/portfolioVladyslav/static/media/personalImage.a6f053d4.jpg"
+  },
+  {
+    "revision": "70fc78e98f33c2bc4b4b181505b12127",
+    "url": "/portfolioVladyslav/static/media/telegram.70fc78e9.svg"
+  },
+  {
+    "revision": "54a72067623400f4f38f0c0371a3f428",
+    "url": "/portfolioVladyslav/static/media/wallet.54a72067.PNG"
+  }
+]);
